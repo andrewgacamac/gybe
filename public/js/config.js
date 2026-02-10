2f26a7680ccadb5bd4edc2da1745656220a5e039
Dev <developer@macbookair.home>
2026-02-10 17:13:10 -0500

    Refactor: Rename admin-portal to public for simpler deployment

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+
+// WARNING: This file contains sensitive keys and is gitignored.
+// Do NOT commit this file.
+
+window.YardGuardConfig = {
+    SUPABASE_URL: 'https://elzshapellgisvsnjgfw.supabase.co',
+    SUPABASE_SERVICE_ROLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsenNoYXBlbGxnaXN2c25qZ2Z3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDQ5NjY5NiwiZXhwIjoyMDg2MDcyNjk2fQ.3fBEUumllij8ywidF-HrrzdGjaqGnqCoMMKc-LObDN4',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsenNoYXBlbGxnaXN2c25qZ2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0OTY2OTYsImV4cCI6MjA4NjA3MjY5Nn0.HmKPNe0TADEkw0F47JeivLa_lUj-jx7PowwbcCOvb3E'
+};
